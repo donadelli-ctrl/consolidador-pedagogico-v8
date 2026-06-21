188bb9708842a142194adbd6c61014da7fa435ea
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 16:50:47 -0300

    Update evolucao.py

--- modulos/evolucao.py
+++ modulos/evolucao.py
@@ -16,7 +16,7 @@ ordem_niveis = {
 
 
 # ==========================================================
-# EVOLUÇÃO
+# EVOLUÇÃO ENTRE DOIS NÍVEIS
 # ==========================================================
 
 def calcular_evolucao(
@@ -103,3 +103,148 @@ def definir_situacao(
     else:
 
         return "Atenção"
+
+
+# ==========================================================
+# GERAR EVOLUÇÃO
+# ==========================================================
+
+def gerar_evolucao(
+
+    base
+
+):
+
+    df = base.copy()
+
+    # ------------------------------------------------------
+    # LP
+    # ------------------------------------------------------
+
+    coluna_lp_final = "PP2_LP_STATUS"
+
+    if "PP3_LP_STATUS" in df.columns:
+
+        coluna_lp_final = "PP3_LP_STATUS"
+
+    df["EVOL_LP"] = (
+
+        df
+
+        .apply(
+
+            lambda x:
+
+            calcular_evolucao(
+
+                x["ADE_LP"],
+
+                x[coluna_lp_final]
+
+            ),
+
+            axis=1
+
+        )
+
+    )
+
+    # ------------------------------------------------------
+    # MAT
+    # ------------------------------------------------------
+
+    coluna_mat_final = "PP2_MAT_STATUS"
+
+    if "PP3_MAT_STATUS" in df.columns:
+
+        coluna_mat_final = "PP3_MAT_STATUS"
+
+    df["EVOL_MAT"] = (
+
+        df
+
+        .apply(
+
+            lambda x:
+
+            calcular_evolucao(
+
+                x["ADE_MAT"],
+
+                x[coluna_mat_final]
+
+            ),
+
+            axis=1
+
+        )
+
+    )
+
+    # ------------------------------------------------------
+    # SITUAÇÃO
+    # ------------------------------------------------------
+
+    df["SITUACAO"] = (
+
+        df
+
+        .apply(
+
+            lambda x:
+
+            definir_situacao(
+
+                x[coluna_lp_final],
+
+                x[coluna_mat_final]
+
+            ),
+
+            axis=1
+
+        )
+
+    )
+
+    evolucao = (
+
+        df
+
+        [
+
+            [
+
+                "TURMA_PAD",
+
+                "NOME",
+
+                "EVOL_LP",
+
+                "EVOL_MAT",
+
+                "SITUACAO"
+
+            ]
+
+        ]
+
+        .copy()
+
+    )
+
+    evolucao.columns = [
+
+        "TURMA",
+
+        "NOME",
+
+        "EVOL LP",
+
+        "EVOL MAT",
+
+        "SITUAÇÃO"
+
+    ]
+
+    return evolucao
