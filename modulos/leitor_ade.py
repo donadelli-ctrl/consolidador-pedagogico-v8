ccad3715d6f6fe6140a97c8eb7503afdb84365f0
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 15:51:41 -0300

    Update leitor_ade.py

--- modulos/leitor_ade.py
+++ modulos/leitor_ade.py
@@ -1,12 +1,72 @@
 import pandas as pd
 
+from modulos.funcoes import (
+
+    padronizar_turma,
+
+    criar_chave
+
+)
+
 
 # ==========================================================
-# LEITOR DA ADE
+# LEITOR ADE
 # ==========================================================
 
 def ler_ADE(caminho):
 
     df = pd.read_excel(caminho)
 
-    return df
+    df.columns = [
+
+        str(col).strip()
+
+        for col in df.columns
+
+    ]
+
+    temp = pd.DataFrame()
+
+    temp["RA"] = df["Id"]
+
+    temp["NOME"] = df["ESTUDANTE"]
+
+    temp["TURMA"] = df["TURMA"]
+
+    temp["ADE_LP"] = df["Status"]
+
+    temp["ADE_MAT"] = df["Status.1"]
+
+    temp["TURMA_PAD"] = (
+
+        temp["TURMA"]
+
+        .apply(
+
+            padronizar_turma
+
+        )
+
+    )
+
+    temp["CHAVE_MERGE"] = (
+
+        temp.apply(
+
+            lambda x:
+
+            criar_chave(
+
+                x["NOME"],
+
+                x["TURMA"]
+
+            ),
+
+            axis=1
+
+        )
+
+    )
+
+    return temp
