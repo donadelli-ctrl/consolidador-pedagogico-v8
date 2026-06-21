3b799a20e2e01e3309b6c7407a6a81b7cb30c12f
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 15:34:57 -0300

    Update leitor_ade.py

--- modulos/leitor_ade.py
+++ modulos/leitor_ade.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+
+# ==========================================================
+# LEITOR DA ADE
+# ==========================================================
+
+def ler_ADE(caminho):
+
+    df = pd.read_excel(caminho)
+
+    return df
