0e55c26361aed21fe6b3d15babd4023a519da3e6
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 16:18:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
+from modulos.leitor_ade import ler_ADE
+from modulos.leitor_pp import ler_PP
+
+from modulos.consolidacao import consolidar_base
+
+from modulos.limpeza import limpar_base
+
+from modulos.participacao import (
+    calcular_participacao,
+    obter_sem_participacao
+)
+
+from modulos.prioritarios import (
+    obter_prioritarios
+)
+
+from modulos.escola_em_numeros import (
+    gerar_resumo_por_turma,
+    gerar_painel_escola
+)
+
+from modulos.excel_final import (
+    montar_abas
+)
+
 
 # ==========================================================
 # CONFIGURAÇÃO
@@ -132,3 +157,76 @@ gerar = st.button(
     "GERAR CONSOLIDADO"
 
 )
+
+# ==========================================================
+# PROCESSAMENTO
+# ==========================================================
+
+if gerar:
+
+    st.success("Processamento iniciado.")
+
+    # ------------------------------------------------------
+    # LEITURA DOS ARQUIVOS
+    # ------------------------------------------------------
+
+    df_ADE = None
+    df_PP1 = None
+    df_PP2 = None
+    df_ADP = None
+    df_PP3 = None
+
+    if arquivo_ADE is not None:
+
+        df_ADE = ler_ADE(
+
+            arquivo_ADE
+
+        )
+
+    if arquivo_PP1 is not None:
+
+        df_PP1 = ler_PP(
+
+            arquivo_PP1,
+
+            "PP1"
+
+        )
+
+    if arquivo_PP2 is not None:
+
+        df_PP2 = ler_PP(
+
+            arquivo_PP2,
+
+            "PP2"
+
+        )
+
+    if arquivo_ADP is not None:
+
+        df_ADP = ler_PP(
+
+            arquivo_ADP,
+
+            "ADP"
+
+        )
+
+    if arquivo_PP3 is not None:
+
+        df_PP3 = ler_PP(
+
+            arquivo_PP3,
+
+            "PP3"
+
+        )
+
+    st.success(
+
+        "Arquivos carregados."
+
+    )
+    
