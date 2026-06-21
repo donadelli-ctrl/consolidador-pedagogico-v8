a591cfbcb977a685a8930dfba9e8d5ef287b9313
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 15:32:32 -0300

    Update funcoes.py

--- modulos/funcoes.py
+++ modulos/funcoes.py
@@ -41,3 +41,78 @@ def normalizar_texto(texto):
     )
 
     return texto
+
+
+# ==========================================================
+# PADRONIZAR TURMAS
+# ==========================================================
+
+def padronizar_turma(turma):
+
+    turma = normalizar_texto(turma)
+
+    m = re.search(
+
+        r"([1-9])[^A-Z]*([A-Z])",
+
+        turma
+
+    )
+
+    if m:
+
+        serie = m.group(1)
+
+        letra = m.group(2)
+
+        return f"{serie}{letra}"
+
+    return ""
+
+
+# ==========================================================
+# CHAVE DE MERGE
+# ==========================================================
+
+def criar_chave(nome, turma):
+
+    return (
+
+        normalizar_texto(nome)
+
+        +
+
+        "_"
+
+        +
+
+        padronizar_turma(turma)
+
+    )
+
+
+# ==========================================================
+# CLASSIFICAÇÃO
+# ==========================================================
+
+def definir_status(valor):
+
+    if pd.isna(valor):
+
+        return "Ausente"
+
+    elif valor < 0.50:
+
+        return "Abaixo do Básico"
+
+    elif valor < 0.70:
+
+        return "Básico"
+
+    elif valor < 0.90:
+
+        return "Adequado"
+
+    else:
+
+        return "Proficiente"
