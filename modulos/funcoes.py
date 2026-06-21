27be4ed83e4264074b9f1926c3887024e4a1d3c7
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 15:30:19 -0300

    Update funcoes.py

--- modulos/funcoes.py
+++ modulos/funcoes.py
@@ -1,1 +1,43 @@
+import pandas as pd
+import unicodedata
+import re
 
+
+# ==========================================================
+# NORMALIZAR TEXTO
+# ==========================================================
+
+def normalizar_texto(texto):
+
+    if pd.isna(texto):
+        return ""
+
+    texto = str(texto).upper().strip()
+
+    texto = ''.join(
+
+        c
+
+        for c in unicodedata.normalize(
+
+            "NFD",
+
+            texto
+
+        )
+
+        if unicodedata.category(c) != "Mn"
+
+    )
+
+    texto = re.sub(
+
+        r"\s+",
+
+        " ",
+
+        texto
+
+    )
+
+    return texto
