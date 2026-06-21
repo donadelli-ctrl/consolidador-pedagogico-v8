d58fc3e58e2828ac2f9469e918daf13c0d7b34f0
donadelli-ctrl <donadelli@prof.educacao.sp.gov.br>
2026-06-21 17:10:06 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+st.set_page_config(
+
+    page_title="Consolidador Pedagógico V8.1",
+
+    layout="wide"
+
+)
+
+st.title(
+
+    "🏆 CONSOLIDADOR PEDAGÓGICO V8.1"
+
+)
+
+st.subheader(
+
+    "URE Pirassununga"
+
+)
+
+st.markdown(
+
+    """
+Bem-vindo ao Consolidador Pedagógico.
+
+Utilize o menu lateral para acessar:
+
+- Consolidador
+- Indicadores
+- Histórico
+- Comparativos
+- Sobre
+"""
+)
